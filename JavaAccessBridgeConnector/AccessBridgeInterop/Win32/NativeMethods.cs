1e4fff7998014e40742a209e887bcb41b251841e
Vadim Zylevich <vzy@leapwork.com>
2018-07-25 10:54:23 +0300

    Init commit

---
+++ JavaAccessBridgeConnector/AccessBridgeInterop/Win32/NativeMethods.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Drawing;
+using System.Runtime.InteropServices;
+
+namespace LeapTest.JavaController.Common.AccessBridgeInterop.Win32 {
+  public static class NativeMethods {
+    [DllImport("user32.dll")]
+    public static extern IntPtr WindowFromPoint(Point p);
+
+    [DllImport("user32.dll")]
+    [return: MarshalAs(UnmanagedType.Bool)]
+    public static extern bool EnumWindows(EnumWindowsProc lpEnumFunc, IntPtr lParam);
+
+    public delegate bool EnumWindowsProc(IntPtr hWnd, IntPtr lParam);
+  }
+}
---
+++ JavaAccessBridgeConnector/App.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+using System.Windows;
+using JavaAccessBridgeConnector.AccessBridgeInterop;
+
+namespace JavaAccessBridgeConnector
+{
+    public partial class App : Application
+    {
+        [DllImport("user32.dll", SetLastError = true)]
+        static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+
+        private AccessBridge ab;
+
+        public App()
+        {
+            ab = new AccessBridge();
+            ab.Initialize();
+        }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            IsJavaWindow();
+            Application.Current.Shutdown();
+        }
+
+        private void IsJavaWindow()
+        {
+            var hWnd = FindWindow("SunAwtFrame", "Calculator");
+            var res = ab.Functions.IsJavaWindow(hWnd);
+            if (!res) return;
+            var res2 = ab.Functions.GetAccessibleContextFromHWND(hWnd, out var vmid, out var ac);
+            MessageBox.Show(res2
+                ? $"Java Virtual Machine Id: {vmid} \n Java Object Handle: {ac.Handle.Value}"
+                : "Java Window not found!");
+        }
+    }
+}
